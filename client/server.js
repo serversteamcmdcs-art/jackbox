26ff6a5560e5d52e223eacb330dc21c28db87ab8
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-03-04 20:15:51 +0500

    Update server.js

--- client/server.js
+++ client/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/index.htm`);
 });
 
 app.listen(3333, () => {
