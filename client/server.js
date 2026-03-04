8faf825638eea77719ba7588510e3434b622b4bf
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-03-04 20:49:41 +0500

    Update server.js

--- client/server.js
+++ client/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/')))
+app.use(express.static(path.join(__dirname, '/gtag')))
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.htm`);
